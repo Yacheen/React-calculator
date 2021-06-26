d72b8ed06008a00947ed831fc3b7b1ae83123bb0
Xenonyy-laptop <gonda.armand@gmail.com>
2021-06-26 13:17:13 +0200

    Deleted CSS and CSS.map, using SCSS directly instead.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import './App.css';
+import './App.scss';
 import ResultComponent from './components/ResultComponent';
 import KeypadComponent from './components/KeypadComponent';
 
